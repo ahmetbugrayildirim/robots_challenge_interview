2b0acd27f78ea9374d902836b451c5c43f48359a
Phillip Nunes <phillipnunes86@gmail.com>
2024-06-05 17:01:24 -0300

    challenge 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import {useState} from 'react';
 import { robots } from "./assets/robots";
 
 type Robot = {
@@ -21,10 +22,32 @@ const Card = ({ id, name, username, email }: Robot) => (
 );
 
 function App() {
+  const [robotsList, setRobotsList] = useState(robots)
+  const [inputValue, setInputValue] = useState('')
+  const handleOnChange = (event: any) => {
+    const value = event.target.value.toLowerCase();
+    setInputValue(value)
+
+    const filteredValues = robotsList.filter(robot => {
+      return robot.name.toLowerCase().includes(value) || robot.email.toLowerCase().includes(value) || robot.username.toLowerCase().includes(value)
+    });
+
+    if (filteredValues.length > 0 && value.length > 0) {
+      setRobotsList(filteredValues)
+      return
+    } else if (value.length > 0 && filteredValues.length === 0){
+      setRobotsList([])
+      return
+    }
+    setRobotsList(robots)
+  }
+
   return (
     <div>
-      <input type="search" placeholder="Search Robots" />
-      <Card {...robots[0]} />
+      <input value={inputValue} type="search" placeholder="Search Robots" onChange={handleOnChange} />
+      {robotsList.map(robot => {
+        return <Card key={robot.id} {...robot} />
+      })}
     </div>
   );
 }
