2fb3c6c12ffa7db8608398786611969226b52dbe
Volodymyr <vl.zinchenko@gmail.com>
2024-06-07 14:22:55 -0400

    Volodymyr Zinchenko

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import { robots } from "./assets/robots";
+import { ChangeEvent, useCallback, useState } from 'react';
 
 type Robot = {
   id: number;
@@ -20,11 +21,35 @@ const Card = ({ id, name, username, email }: Robot) => (
   </div>
 );
 
+function hasSearch(input: string, search: string): boolean {
+  return Boolean(input?.toLowerCase().includes(search.toLowerCase()))
+}
+
+function filterBySearch(robot: Robot, search: string): boolean {
+  if (!search) {
+    return true;
+  }
+
+  return hasSearch(robot.name, search)
+    || hasSearch(robot.email, search)
+    || hasSearch(robot.username, search);
+}
+
+
 function App() {
+
+  const [search, setSearch] = useState<string>('');
+
+  const handleSearchChange = useCallback((evt: ChangeEvent<HTMLInputElement> ) => {
+    setSearch(evt.target?.value ?? '');
+  }, [])
+
+  const filteredRobots = robots.filter((robot) => filterBySearch(robot, search))
+
   return (
     <div>
-      <input type="search" placeholder="Search Robots" />
-      <Card {...robots[0]} />
+      <input type="search" placeholder="Search Robots" onChange={handleSearchChange} />
+      {filteredRobots.map(robot => (<Card {...robot}/>))}
     </div>
   );
 }
