f7ab729e3aaccc23b0aadd8000855ca540868d21
Stephen Pearce <stephen.r.pearce@gmail.com>
2024-06-11 12:00:51 -0400

    first challenge

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import { robots } from "./assets/robots";
+import { useState } from "react";
 
 type Robot = {
   id: number;
@@ -21,10 +22,24 @@ const Card = ({ id, name, username, email }: Robot) => (
 );
 
 function App() {
+  const [query, setQuery] = useState('');
+  const [filteredBots, setFilteredBots] = useState(robots);
+
+  const updateQuery = (value: string) => {
+    setQuery(value);
+
+    const lowerValue = value.toLowerCase()
+    const matches = robots.filter(({ name, email, username }) => name.toLowerCase().indexOf(lowerValue) !== -1 || 
+      email.toLowerCase().indexOf(lowerValue) !== -1 || username.toLowerCase().indexOf(lowerValue) !== -1)
+    setFilteredBots(matches)
+  }
+
   return (
     <div>
-      <input type="search" placeholder="Search Robots" />
-      <Card {...robots[0]} />
+      <input type="search" placeholder="Search Robots" onChange={e => updateQuery(e.currentTarget.value)} value={query} />
+      {filteredBots.map(robot => 
+        <Card key={robot.id} {...robot} />
+      )}
     </div>
   );
 }
